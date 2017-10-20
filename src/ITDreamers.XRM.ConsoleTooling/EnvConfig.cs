f9e5fdb7fb537df7cb37f972b5203598728ae81d
Constantine Tsvigun <kostyantyn.tsvigun@gmail.com>
2017-10-20 14:44:42 +0300

    Implemented solution structure + added classes

---
+++ src/ITDreamers.XRM.ConsoleTooling/EnvConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITDreamers.XRM.ConsoleTooling
+{
+    public class EnvConfig
+    {
+        protected string _variablesGroupKey = "YOUR_ORG";
+        protected IDictionary _envVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User);
+
+        public EnvConfig(string variablesGroupKey)
+        {
+            _variablesGroupKey = variablesGroupKey;
+        }
+
+        public string D365OrgUrl => _envVariables[$"{_variablesGroupKey}_D365_URL"].ToString();
+        public string D365OrgLgn => _envVariables[$"{_variablesGroupKey}_D365_LGN"].ToString();
+        public string D365OrgPswd => _envVariables[$"{_variablesGroupKey}_D365_PSWD"].ToString();
+
+        public string CrmConnectionString => $"Url={D365OrgUrl};Username={D365OrgLgn};Password={D365OrgPswd};AuthType=Office365;";
+    }
+}
